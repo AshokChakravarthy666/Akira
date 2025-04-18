a93c922932f849ff4a088696145191238cbcbde0
𝙰𝚂𝙷𝙾𝙺𝚇𝙶𝙷𝙾𝚂𝚃 <106798009+AshokChakravarthy666@users.noreply.github.com>
2025-04-18 19:34:44 +0530

    Update __main__.py

--- Akira/__main__.py
+++ Akira/__main__.py
@@ -1,16 +1,15 @@
-import asyncio
 from multiprocessing import Process
-from Akira.bot import main
-from web import run_web
+from Akira.bot import start_bot
+from Akira.web import run_flask
 
 if __name__ == "__main__":
     try:
         # Start Flask web server in a separate process
-        flask_process = Process(target=run_web)
+        flask_process = Process(target=run_flask)
         flask_process.start()
 
-        # Run the bot asynchronously
-        asyncio.run(main())
+        # Start the Pyrogram bot
+        start_bot()
 
     except Exception as e:
         print(f"Error while starting bot or Flask: {e}")
